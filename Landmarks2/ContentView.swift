a530a38ef082cc762cb94c6f8538e1cb374d5c6d
shinespark <shnsprk@gmail.com>
2022-05-28 07:36:13 +0900

    Initial Commit

---
+++ Landmarks2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks2
+//
+//  Created by shinespark on 2022/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks2/Landmarks2App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks2App.swift
+//  Landmarks2
+//
+//  Created by shinespark on 2022/05/28.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
