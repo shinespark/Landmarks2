48b40e4a11497c2192f3c2036638cf17d2f7072f
shinespark <shnsprk@gmail.com>
2022-05-29 01:43:56 +0900

    finish Section 7

--- Landmarks2/Views/LandmarkList.swift
+++ Landmarks2/Views/LandmarkList.swift
@@ -5,7 +5,7 @@ struct LandmarkList: View {
         NavigationView {
             List(landmarks) { landmark in
                 NavigationLink{
-                    LandmarkDetail()
+                    LandmarkDetail(landmark: landmark)
                 } label: {
                     LandmarkRow(landmark: landmark)
                 }
